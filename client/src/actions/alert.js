af433e9ba9ef2648dbee860aaa395ec65ad89020
Mohammad Palash Babu <mdpalashbabu4@gmail.com>
2020-04-06 11:55:59 +0600

    v3

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -3,7 +3,7 @@ import { v4 as uuid } from "uuid";
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 export const setAlert = (msg, alertType, timeout = 3000) => dispatch => {
-    const id = v4();
+    const id = uuid();
     // const id = uuid.v4();
     dispatch({
         type: SET_ALERT,
